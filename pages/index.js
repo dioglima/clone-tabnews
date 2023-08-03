fc2d59be2f1f3040dc23b813f462cadc003aa616
Diogo Lima <diogo.ll.lima08@gmail.com>
2023-08-03 10:37:11 -0300

    mensagem samuel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home(){
-    return <h1>Vai Comprar Pão</h1>
+    return <h1>Irmão, eu concordo com você sobre tudo, sobre se 
+        desenvolver em russas, investimento aqui, tudo, oque me preocupa
+        é que você sempre demonstrou amar lá, gostar da rotina, dos amigos,
+        e do rumo que você estava trilhando, e tudo isso mudou dps da clara.
+            Eu só espero que você esteja tomando essa decisão baseado no que você quer, 
+        e não só pra fugir de uma menina.
+            Digo isso porque ano passado eu queria muito voltar pra fortaleza e eu falava tudo que você falou,
+        de gastos, de ter mais oportunidade e tals, mas no fundo mesmo era só pra ficar mais perto da Sofia.
+            Oque você decidir eu vou apoiar, só peço que você tome cuidado pra nn tomar uma decisão precipitada, então pense bem,
+        quando voltar as aulas, sua mente vai clarear mais, e se você decidir voltar mesmo pra Fortaleza, tá otimo, tenho certeza
+        que você se dará muito bem aqui.
+    </h1>
 }
 
 export default Home;
